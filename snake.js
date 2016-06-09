4a07ead11714ba700784e3e769be0c913d4df4d3
VJCNIC001 <vjcnic001@myuct.ac.za>
2016-06-09 13:10:30 +0200

    Snake block moves

--- snake.js
+++ snake.js
@@ -1,6 +1,26 @@
-//Represents snake
-var snake = [{ top: 0, left: 0}];
+var drawSnake = function(snakeToDraw) 
+{
+	//Defines the length and colour of snake
+	var drawableSnake = { color: "yellow", pixels: snake };
+	//Array/object drawn
+	var drawableObjects = [drawableSnake];
+	CHUNK.draw(drawableObjects);
+}
 
-var drawableSnake = { color: "green", pixels: snake };
-var drawableObjects = [drawableSnake];
-CHUNK.draw(drawableObjects);
+var moveSnake = function(snake) 
+{
+  var oldSegment = snake[0];
+  var newSegment = { top: oldSegment.top + 1, left: oldSegment.left };
+  var newSnake = [newSegment];
+  return newSnake;
+}
+
+var advanceGame = function() 
+{
+  snake = moveSnake(snake);
+  drawSnake(snake);
+}
+
+//Represents snake. Creates length of snake using array/hash map
+var snake = [{ top: 0, left: 0}];
+CHUNK.executeNTimesPerSecond(advanceGame, 1);
